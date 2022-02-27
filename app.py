a7be9c76ae43b66ed335116f3e44de125f1fc661
DaveIshaSnehalbhai <96402991+DaveIshaSnehalbhai@users.noreply.github.com>
2022-02-27 22:43:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title('Diabeties Prediction System')
 s0 = st.number_input('Pregnancies')
 s1 = st.number_input('Glucose')
-s2 = st.number_input('BloodPressure')
+s2 = st.number_input('BloodPressure')  
 s3 = st.number_input('SkinThickness')
 s4 = st.number_input('BMI')
 s6 = st.number_input('DiabetesPedigreeFunction')  
